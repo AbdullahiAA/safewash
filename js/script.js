6c3c14bce91f1ed7d1fcd18733d5686f1b78c11d
Abdullah <osuolaleabdullahi@gmail.com>
2021-05-08 14:49:37 +0100

    White Spaces Adjusted (Mobile)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let menuIcon = document.querySelector(".menu-icon")
 let nav = document.querySelector("nav")
+let body = document.querySelector('body')
 
 menuIcon.addEventListener("click", () => {
     nav.classList.toggle('open');
     menuIcon.classList.toggle('active');
+    body.classList.toggle('navbar-open');
 })
