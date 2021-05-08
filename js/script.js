912529e4e84c85599cd1e7f591ba25678a0380bb
Abdullah <osuolaleabdullahi@gmail.com>
2021-05-08 18:28:16 +0100

    Distributors Page Added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-let menuIcon = document.querySelector(".menu-icon")
-let nav = document.querySelector("nav")
-let body = document.querySelector('body')
+const menuIcon = document.querySelector(".menu-icon")
+const nav = document.querySelector("nav")
+const navLinks = document.querySelectorAll("nav li a")
+const body = document.querySelector('body')
+const header = document.querySelector('header')
+const main = document.querySelector('main')
 
-menuIcon.addEventListener("click", () => {
+
+// NavBar Toggle Handler
+const navbarToggleHandler = () => {
     nav.classList.toggle('open');
     menuIcon.classList.toggle('active');
     body.classList.toggle('navbar-open');
+}
+
+
+menuIcon.addEventListener("click", () => {
+    navbarToggleHandler()
+})
+
+navLinks.forEach((navLink => {
+    navLink.addEventListener("click", () => {
+        if (nav.classList.value === 'open') {
+            navbarToggleHandler()
+        }
+    })
+}))
+
+// Add Shadow to Header
+window.addEventListener("scroll", () => {
+    let scrollPosition = window.pageYOffset;
+
+    if (scrollPosition > 0) {
+        header.classList.add('on-scroll')
+    } else {
+        header.classList.remove('on-scroll')
+    }
 })
